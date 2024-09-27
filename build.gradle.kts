d0e27971ccf4f691e3e3e3701876af702a2368a5
willian-d29 <willianluis345@gmail.com>
2024-09-26 21:09:42 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    kotlin("jvm")
+}
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+repositories {
+}
+kotlin {
+    jvmToolchain(8)
+}
